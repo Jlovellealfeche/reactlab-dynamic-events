f37c2c4f7cf7ddab149547d13e0ec2d107d288d1
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-03-16 15:00:56 +0100

    step 2 added text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ function App() {
   return (
     <div>
       <h1>Task: Add a button and handle a click event</h1>
+
+      <button>Guess the number between 1 and 3</button>
+    
     </div>
   );
 }
